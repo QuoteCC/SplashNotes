2a4a2a98afc79d36a7e8169def954dacdc49367d
QuoteCC <colin.corliss@gmail.com>
2017-02-27 10:17:29 -0500

    Inital add of all project files

---
+++ app/src/main/java/io/github/quotecc/splash/NoteScreen.java
@@ -0,0 +1,47 @@
+package io.github.quotecc.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class NoteScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_screen);
+
+        Toolbar stylBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(stylBar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_settings:
+                return true;
+            case R.id.action_save:
+                Toast.makeText(NoteScreen.this,"Save",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.action_delete:
+                break;
+            default:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/io/github/quotecc/splash/Splash.java
@@ -0,0 +1,29 @@
+package io.github.quotecc.splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, NoteScreen.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        Timer op = new Timer();
+        op.schedule(task, 3000);
+
+
+    }
+}
